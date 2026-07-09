b2f45bc32d2dfd26691e7618767d76665a95a806
Greg Dorrell <Greg@memimo.co.uk>
2026-07-09 20:30:05 +0100

    Optimize BSON case class decoding
    (cherry picked from commit dff08112ee29ae39c45a399a0b90edb6426c3db7)

--- reactivemongo-bson-derivation/src/main/scala/hearth/kindlings/reactivemongobsonderivation/internal/runtime/BsonDocumentHandlerFactories.scala
+++ reactivemongo-bson-derivation/src/main/scala/hearth/kindlings/reactivemongobsonderivation/internal/runtime/BsonDocumentHandlerFactories.scala
@@ -1,7 +1,7 @@
 package hearth.kindlings.reactivemongobsonderivation.internal.runtime
 
 import reactivemongo.api.bson.BSONDocument
-import scala.util.{Failure, Success, Try}
+import scala.util.Try
 
 object BsonDocumentHandlerFactories {
 
@@ -14,18 +14,4 @@ object BsonDocumentHandlerFactories {
       def readDocument(doc: BSONDocument): Try[A] = readFn(doc)
       def writeTry(value: A): Try[BSONDocument] = writeFn(value)
     }
-
-  /** Sequences a list of [[scala.util.Try]] values and constructs a result. */
-  def sequenceTries[T](tries: List[Try[Any]], construct: Array[Any] => T): Try[T] = {
-    val result = new Array[Any](tries.size)
-    var i = 0
-    while (i < tries.size) {
-      tries(i) match {
-        case Success(v)    => result(i) = v
-        case f: Failure[?] => return f.asInstanceOf[Failure[T]]
-      }
-      i += 1
-    }
-    Success(construct(result))
-  }
 }
