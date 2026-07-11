4f36da4c70f7caeb840725a2e01f41fbb7370788
Greg Dorrell <Greg@memimo.co.uk>
2026-07-11 03:06:19 +0100

    Support Scala 3 BSON derives syntax
    (cherry picked from commit 9dc60bb453bf80fef644375b0e3636ff895be983)

--- reactivemongo-bson-derivation/src/test/scala-3/hearth/kindlings/reactivemongobsonderivation/BsonDocumentHandlerScala3Spec.scala
+++ reactivemongo-bson-derivation/src/test/scala-3/hearth/kindlings/reactivemongobsonderivation/BsonDocumentHandlerScala3Spec.scala
@@ -3,7 +3,21 @@ package hearth.kindlings.reactivemongobsonderivation
 import hearth.MacroSuite
 import reactivemongo.api.bson.BSONDocument
 
+final case class DerivesPerson(name: String, age: Int) derives KindlingsBsonDocumentHandler
+
 final class BsonDocumentHandlerScala3Spec extends MacroSuite {
+
+  group("Scala 3 derives") {
+
+    test("derives a Kindlings BSON document handler subtype") {
+      val value = DerivesPerson("Alice", 30)
+      val document = BSONDocument("name" -> "Alice", "age" -> 30)
+      val handler = summon[KindlingsBsonDocumentHandler[DerivesPerson]]
+
+      assertEquals(handler.writeTry(value).get, document)
+      assertEquals(handler.readDocument(document).get, value)
+    }
+  }
 
   group("named tuples (Scala 3.7+)") {
 
